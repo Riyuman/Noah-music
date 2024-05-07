bce0244d0998e1039172a61e9f305a373b74566f
Riyuman <139713314+Riyuman@users.noreply.github.com>
2024-05-07 22:24:31 +0530

    Update play.py

--- VenomX/utils/inline/play.py
+++ VenomX/utils/inline/play.py
@@ -41,25 +41,25 @@ def stream_markup_timer(_, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 < umm <= 10:
-        bar = "◉—————————"
+        bar = "ɴ—————————"
     elif 10 < umm < 20:
-        bar = "—◉————————"
+        bar = "—ɪ————————"
     elif 20 <= umm < 30:
-        bar = "——◉———————"
+        bar = "——ᴄ———————"
     elif 30 <= umm < 40:
-        bar = "———◉——————"
+        bar = "———ᴋ——————"
     elif 40 <= umm < 50:
-        bar = "————◉—————"
+        bar = "————♡—————"
     elif 50 <= umm < 60:
-        bar = "—————◉————"
+        bar = "—————ɴ————"
     elif 60 <= umm < 70:
-        bar = "——————◉———"
+        bar = "——————ᴏ———"
     elif 70 <= umm < 80:
-        bar = "———————◉——"
+        bar = "———————ᴀ——"
     elif 80 <= umm < 95:
-        bar = "————————◉—"
+        bar = "————————ᴀ—"
     else:
-        bar = "—————————◉"
+        bar = "—————————♡"
     buttons = [
         [
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
